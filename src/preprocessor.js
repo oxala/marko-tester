cbf0eeb07adb92d0ef50bbd887eb5f32937009cd
Gleb Ryshkov <gryshkov@ebay.com>
2018-10-06 12:04:14 -0700

    refactor: moved out shallow function

--- src/preprocessor.js
+++ src/preprocessor.js
@@ -1,22 +1,24 @@
 const { taglibFinder, taglibLookup, compileFile } = require('marko/compiler');
 const { markoVersion } = require('./versions');
 
+const shallowPatch = () => {
+  if (markoVersion === 3) {
+    try {
+      taglibLookup.registerTaglib(require.resolve('marko-widgets/marko'));
+    } catch (error) {
+      console.warn('No marko-widgets were found.');
+    }
+  }
+
+  taglibFinder.find = (a, b) => b;
+};
+
 module.exports = {
   process(src, filepath, { globals = {} }) {
     const { tester = {} } = globals;
     const { shallow = true } = tester;
 
-    if (shallow) {
-      if (markoVersion === 3) {
-        try {
-          taglibLookup.registerTaglib(require.resolve('marko-widgets/marko'));
-        } catch (error) {
-          console.warn('No marko-widgets were found.');
-        }
-      }
-
-      taglibFinder.find = (a, b) => b;
-    }
+    if (shallow) shallowPatch();
 
     return (markoVersion === 3)
       ? compileFile(filepath)
