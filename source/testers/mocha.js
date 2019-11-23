343cdc3db4c95097a6d0c813f133cf4fc87f6268
Dand <dand@lm-sjc-11010440.corp.ebay.com>
2019-11-22 16:01:50 -0800

    fix: added global variable

--- source/testers/mocha.js
+++ source/testers/mocha.js
@@ -17,7 +17,7 @@ var mocha = new Mocha({
       mochaFile: './.coverage/junit.xml'
     }
   },
-  globals: ['document', 'window', 'GLOBAL_LASSO', '$W10NOOP', 'version']
+  globals: ['document', 'window', 'GLOBAL_LASSO', '$W10NOOP', 'version', '__RAPTOR_PUBSUB']
 });
 
 function testMocha(done) {
